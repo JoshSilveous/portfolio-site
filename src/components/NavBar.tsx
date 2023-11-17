aa0f89b53defe6dd7c6268c8168f63854256b7b1
Joshua Silveous <JoshSilveous@gmail.com>
2023-11-17 11:49:37 -0500

    updated styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { stringToSpanCharArray, stringToSpanWordArray } from '../functions'
 import './NavBar.scss'
 export function NavBar() {
@@ -17,7 +17,8 @@ export function NavBar() {
 			window.removeEventListener('scroll', handleFullscreenChange)
 		}
 	}, [])
-
+	// need to fix height of paragraphs when scrolling down.
+	// maybe make a function that can calculate the client height of an element?
 	const splashParagraph =
 		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ' +
 		'do eiusmod tempor incididunt ut labore et dolore magna ' +
@@ -33,19 +34,39 @@ export function NavBar() {
 		'in massa tempor nec. Fermentum et sollicitudin ac orci ' +
 		'phasellus egestas tellus. Nisl tincidunt eget nullam non ' +
 		'nisi est sit amet facilisis.'
+	const testRef = useRef<HTMLParagraphElement>(null)
+	useEffect(() => {
+		const timeoutID = setTimeout(() => {
+			testRef.current!.classList.add('height-animation-triggered')
+			clearTimeout(timeoutID)
+		}, 2000)
+	}, [])
+
 	console.log(stringToSpanWordArray(splashParagraph))
 	return (
 		<div className={`navbar ${isFullscreen ? 'fullscreen' : ''}`}>
 			<div className='splash-info'>
 				<h1>{stringToSpanCharArray('Joshua Silveous', 0, 0.05)}</h1>
-				<p>{stringToSpanWordArray(splashParagraph, 1, 0.015)}</p>
+				<p ref={testRef}>
+					{stringToSpanWordArray(splashParagraph, 2, 0.015)}
+				</p>
 			</div>
 			<div className='anchor-container'>
-				<div className='anchor-link'>Example A</div>
-				<div className='anchor-link'>Example B</div>
-				<div className='anchor-link'>Example C</div>
-				<div className='anchor-link'>Example D</div>
-				<div className='anchor-link'>Example E</div>
+				<div className='anchor-link' style={{ animationDelay: '4.8s' }}>
+					Example A
+				</div>
+				<div className='anchor-link' style={{ animationDelay: '4.6s' }}>
+					Example B
+				</div>
+				<div className='anchor-link' style={{ animationDelay: '4.4s' }}>
+					Example C
+				</div>
+				<div className='anchor-link' style={{ animationDelay: '4.6s' }}>
+					Example D
+				</div>
+				<div className='anchor-link' style={{ animationDelay: '4.8s' }}>
+					Example E
+				</div>
 			</div>
 		</div>
 	)
