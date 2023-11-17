875710255566b0b5a11c0b619ed437956fdef128
Joshua Silveous <JoshSilveous@gmail.com>
2023-11-17 11:24:00 -0500

    updated styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,8 @@
 import { useEffect, useState } from 'react'
+import { stringToSpanCharArray, stringToSpanWordArray } from '../functions'
 import './NavBar.scss'
 export function NavBar() {
 	const [isFullscreen, setIsFullscreen] = useState(true)
-
-	function stringToSpanArray(string: string) {
-		let animationDelay = 0
-		const arr = Array.from(string).map((letter, index) => {
-			const thisAnimationDelay = animationDelay
-			animationDelay += 0.1
-			return (
-				<span
-					key={index}
-					style={{ animationDelay: `${thisAnimationDelay}s` }}
-				>
-					{letter}
-				</span>
-			)
-		})
-		return arr
-	}
 
 	useEffect(() => {
 		function handleFullscreenChange() {
@@ -34,11 +18,27 @@ export function NavBar() {
 		}
 	}, [])
 
+	const splashParagraph =
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ' +
+		'do eiusmod tempor incididunt ut labore et dolore magna ' +
+		'aliqua. Ullamcorper morbi tincidunt ornare massa eget ' +
+		'egestas purus viverra. Ante in nibh mauris cursus mattis ' +
+		'molestie a. Faucibus turpis in eu mi. Eu feugiat pretium ' +
+		'nibh ipsum consequat. Rhoncus mattis rhoncus urna neque ' +
+		'viverra justo nec. Velit dignissim sodales ut eu sem integer ' +
+		'vitae justo eget. Facilisi etiam dignissim diam quis enim. ' +
+		'Rhoncus dolor purus non enim praesent elementum facilisis ' +
+		'leo. In pellentesque massa placerat duis ultricies lacus ' +
+		'sed. Mus mauris vitae ultricies leo. Aliquam faucibus purus ' +
+		'in massa tempor nec. Fermentum et sollicitudin ac orci ' +
+		'phasellus egestas tellus. Nisl tincidunt eget nullam non ' +
+		'nisi est sit amet facilisis.'
+	console.log(stringToSpanWordArray(splashParagraph))
 	return (
 		<div className={`navbar ${isFullscreen ? 'fullscreen' : ''}`}>
 			<div className='splash-info'>
-				<h1>{stringToSpanArray('Joshua Silveous')}</h1>
-				<p>Blah blah blah</p>
+				<h1>{stringToSpanCharArray('Joshua Silveous', 0, 0.05)}</h1>
+				<p>{stringToSpanWordArray(splashParagraph, 1, 0.015)}</p>
 			</div>
 			<div className='anchor-container'>
 				<div className='anchor-link'>Example A</div>
