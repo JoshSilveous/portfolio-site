93be241543c127a08747bb4a832e9cb0369435ab
Joshua Silveous <JoshSilveous@gmail.com>
2023-11-18 14:12:52 -0500

    abstracted large text block to separate file

---
+++ src/components/SPLASH_PARAGRAPH.ts
@@ -0,0 +1,15 @@
+export const SPLASH_PARAGRAPH =
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ' +
+	'do eiusmod tempor incididunt ut labore et dolore magna ' +
+	'aliqua. Ullamcorper morbi tincidunt ornare massa eget ' +
+	'egestas purus viverra. Ante in nibh mauris cursus mattis ' +
+	'molestie a. Faucibus turpis in eu mi. Eu feugiat pretium ' +
+	'nibh ipsum consequat. Rhoncus mattis rhoncus urna neque ' +
+	'viverra justo nec. Velit dignissim sodales ut eu sem integer ' +
+	'vitae justo eget. Facilisi etiam dignissim diam quis enim. ' +
+	'Rhoncus dolor purus non enim praesent elementum facilisis ' +
+	'leo. In pellentesque massa placerat duis ultricies lacus ' +
+	'sed. Mus mauris vitae ultricies leo. Aliquam faucibus purus ' +
+	'in massa tempor nec. Fermentum et sollicitudin ac orci ' +
+	'phasellus egestas tellus. Nisl tincidunt eget nullam non ' +
+	'nisi est sit amet facilisis.'
