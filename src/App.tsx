75ea5ed930407fc4a7db897410cee3ea5476c158
Joshua Silveous <JoshSilveous@gmail.com>
2023-11-28 16:01:51 -0500

    worked on NavMobile component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.scss'
 import { NavDesktop } from './components/Nav/NavDesktop/NavDesktop'
 import { Main } from './components/Main/Main'
+import { NavMobile } from './components/Nav/NavMobile/NavMobile'
 
 function App() {
 	return (
 		<div className='app'>
 			<NavDesktop />
+			<NavMobile />
 			<Main />
 		</div>
 	)
