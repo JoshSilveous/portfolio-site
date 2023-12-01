4cad7e4b88724b9806d766840a6a28b02a48222c
Joshua Silveous <JoshSilveous@gmail.com>
2023-12-01 11:19:19 -0500

    shortened example paragraph

--- src/components/SPLASH_PARAGRAPH.ts
+++ src/components/SPLASH_PARAGRAPH.ts
@@ -6,10 +6,4 @@ export const SPLASH_PARAGRAPH =
 	'molestie a. Faucibus turpis in eu mi. Eu feugiat pretium ' +
 	'nibh ipsum consequat. Rhoncus mattis rhoncus urna neque ' +
 	'viverra justo nec. Velit dignissim sodales ut eu sem integer ' +
-	'vitae justo eget. Facilisi etiam dignissim diam quis enim. ' +
-	'Rhoncus dolor purus non enim praesent elementum facilisis ' +
-	'leo. In pellentesque massa placerat duis ultricies lacus ' +
-	'sed. Mus mauris vitae ultricies leo. Aliquam faucibus purus ' +
-	'in massa tempor nec. Fermentum et sollicitudin ac orci ' +
-	'phasellus egestas tellus. Nisl tincidunt eget nullam non ' +
-	'nisi est sit amet facilisis.'
+	'vitae justo eget. Facilisi etiam dignissim diam quis enim. '
