8e0a8d02070187896fcef028f628da842af7658f
Joshua Silveous <JoshSilveous@gmail.com>
2023-12-20 15:25:51 -0500

    dissolved Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.scss'
-import { Main } from './components/Main/Main'
 import { Navbar } from './components/NavBar/Navbar'
+import { AboutMe, ContactInfo, Experience, Projects, Skills } from './components/Main/sections'
 
 function App() {
 	return (
 		<div className='app'>
 			<Navbar />
-			<Main />
+
+			<div className='main'>
+				<AboutMe />
+				<Skills />
+				<Projects />
+				<Experience />
+				<ContactInfo />
+			</div>
 		</div>
 	)
 }
