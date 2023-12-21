62fb3059dcebd1f82448d2b72c00a1764b15b753
Joshua Silveous <JoshSilveous@gmail.com>
2023-12-21 12:55:22 -0500

    added documentation for FoldComponent

--- src/components/FoldComponent/FoldComponent.tsx
+++ src/components/FoldComponent/FoldComponent.tsx
@@ -1,23 +1,48 @@
-import { ReactNode, useState } from 'react'
+import { ReactNode } from 'react'
 import './FoldComponent.scss'
 import { FoldIcon } from '../../assets'
 
-interface FoldComponentProps {
-	title: string
-	children?: ReactNode
-}
-export function FoldComponent({ title, children }: FoldComponentProps) {
-	const [isFolded, setIsFolded] = useState(true)
+/**
+ * A component that can be folded open and closed. This component does NOT manage state.
+ *
+ * **REQUIRED PROPS**
+ *
+ * `title` *string* - The title of the fold, displayed in the header
+ *
+ * `folded` *boolean* - Boolean value controlling if the content is hidden or not
+ *
+ * `handleToggle` *Function* - Function that will be executed whenever the header is clicked.
+ * *Should toggle* `folded`
+ */
+export function FoldComponent({ title, children, folded, handleToggle }: FoldComponentProps) {
 	return (
 		<div className='fold-component'>
-			<div
-				className={`fold-title${isFolded ? ' folded' : ''}`}
-				onClick={() => setIsFolded((p) => !p)}
-			>
+			<div className={`fold-title${folded ? ' folded' : ''}`} onClick={handleToggle}>
 				<FoldIcon />
 				<h2>{title}</h2>
 			</div>
 			<div className='fold-content'>{children}</div>
 		</div>
 	)
+}
+
+interface FoldComponentProps {
+	/**
+	 * The title of the fold, displayed in the header
+	 */
+	title: string
+	/**
+	 * The content hidden by the fold
+	 */
+	children?: ReactNode
+	/**
+	 * Boolean value controlling if the content is hidden or not
+	 */
+	folded: boolean
+	/**
+	 * Function that will be executed whenever the header is clicked.
+	 *
+	 * *Should toggle* `folded`
+	 */
+	handleToggle: () => void
 }
