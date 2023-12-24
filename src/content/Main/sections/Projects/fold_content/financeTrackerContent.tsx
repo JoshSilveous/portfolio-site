b3cb36c429bad7817b67f1f073c058bc5868f3ad
Joshua Silveous <JoshSilveous@gmail.com>
2023-12-23 20:50:39 -0500

    added content to projects

--- src/content/Main/sections/Projects/fold_content/financeTrackerContent.tsx
+++ src/content/Main/sections/Projects/fold_content/financeTrackerContent.tsx
@@ -1,6 +1,78 @@
+import {
+	ExpressIcon,
+	GitHubIcon,
+	JestIcon,
+	NodeIcon,
+	ReactIcon,
+	SQLIcon,
+	SassIcon,
+	TypeScriptIcon,
+} from '../../../../../assets'
+
 export const financeTrackerContent = (
-	<>
-		Some text Some text Some text Some text Some text Some text Some text Some text Some text
-		Some text Some text Some text Some text Some text Some text Some text Some text Some text
-	</>
+	<div className='fold guitar-visualizer'>
+		<div className='links-container'>
+			<a href='https://github.com/JoshSilveous/money-tracker-server' target='_blank'>
+				<GitHubIcon />
+				GitHub (Server)
+			</a>
+			<a href='https://github.com/JoshSilveous/money-tracker-client' target='_blank'>
+				<GitHubIcon />
+				GitHub (Frontend)
+			</a>
+		</div>
+		<div className='description'>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				incididunt ut labore et dolore magna aliqua. Vulputate eu scelerisque felis
+				imperdiet proin fermentum leo vel. Porttitor eget dolor morbi non arcu. At risus
+				viverra adipiscing at in tellus integer feugiat. Sociis natoque penatibus et magnis.
+				Risus quis varius quam quisque. Enim ut sem viverra aliquet eget. Tempor id eu nisl
+				nunc. Amet dictum sit amet justo donec. Faucibus nisl tincidunt eget nullam non. Ac
+				felis donec et odio pellentesque diam volutpat. Mattis ullamcorper velit sed
+				ullamcorper.
+			</p>
+			<p>
+				Et netus et malesuada fames ac turpis. Non enim praesent elementum facilisis leo
+				vel. Nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque.
+				Pretium nibh ipsum consequat nisl vel. Viverra aliquet eget sit amet. Nulla aliquet
+				porttitor lacus luctus accumsan tortor posuere ac. Eleifend quam adipiscing vitae
+				proin sagittis nisl rhoncus mattis. Blandit massa enim nec dui nunc mattis enim ut
+				tellus. Sollicitudin tempor id eu nisl nunc mi. Ac turpis egestas integer eget.
+				Viverra nam libero justo laoreet sit amet cursus sit amet. Amet dictum sit amet
+				justo donec enim.
+			</p>
+		</div>
+		<div className='tech-stack'>
+			<h3>Tech Stack</h3>
+			<div className='item'>
+				<TypeScriptIcon />
+				TypeScript
+			</div>
+			<div className='item'>
+				<ReactIcon />
+				React
+			</div>
+			<div className='item'>
+				<SassIcon />
+				Sass
+			</div>
+			<div className='item'>
+				<SQLIcon />
+				SQL
+			</div>
+			<div className='item'>
+				<NodeIcon />
+				Node
+			</div>
+			<div className='item'>
+				<ExpressIcon />
+				Express
+			</div>
+			<div className='item'>
+				<JestIcon />
+				Jest
+			</div>
+		</div>
+	</div>
 )
