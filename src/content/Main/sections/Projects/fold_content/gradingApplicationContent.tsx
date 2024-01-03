6a7e86b5f6562a9e2465c7c17cd34f9cb345b5b3
Joshua Silveous <JoshSilveous@gmail.com>
2024-01-03 16:19:37 -0500

    Added in grading application content

--- src/content/Main/sections/Projects/fold_content/gradingApplicationContent.tsx
+++ src/content/Main/sections/Projects/fold_content/gradingApplicationContent.tsx
@@ -1,6 +1,59 @@
+import {
+	ElectronIcon,
+	GitHubIcon,
+	NodeIcon,
+	PDFIcon,
+	ReactIcon,
+	SQLIcon,
+	SassIcon,
+	TypeScriptIcon,
+} from '../../../../../assets'
+
 export const gradingApplicationContent = (
-	<>
-		Some text Some text Some text Some text Some text Some text Some text Some text Some text
-		Some text Some text Some text Some text Some text Some text Some text Some text Some text
-	</>
+	<div className='fold guitar-visualizer'>
+		<div className='col'>
+			<div className='links'>
+				<h3>Links</h3>
+				<a href='https://github.com/JoshSilveous/grading-application' target='_blank'>
+					<GitHubIcon />
+					GitHub
+				</a>
+				<a
+					href='https://github.com/JoshSilveous/grading-application/blob/master/Report.pdf'
+					target='_blank'
+				>
+					<PDFIcon />
+					Report
+				</a>
+			</div>
+			<div className='tech'>
+				<h3>Technologies</h3>
+				<div className='item'>
+					<TypeScriptIcon />
+					TypeScript
+				</div>
+				<div className='item'>
+					<ReactIcon />
+					React
+				</div>
+				<div className='item'>
+					<SassIcon />
+					Sass
+				</div>
+				<div className='item'>
+					<ElectronIcon />
+					Electron
+				</div>
+				<div className='item'>
+					<NodeIcon />
+					Node
+				</div>
+				<div className='item'>
+					<SQLIcon />
+					SQL
+				</div>
+			</div>
+		</div>
+		<div className='description'></div>
+	</div>
 )
