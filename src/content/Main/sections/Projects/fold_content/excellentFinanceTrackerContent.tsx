6f614def8e69701c57b4b62883ec2029507e6f29
Joshua Silveous <JoshSilveous@gmail.com>
2024-01-10 21:04:36 -0500

    added icons to excellentFinanceTracker

--- src/content/Main/sections/Projects/fold_content/excellentFinanceTrackerContent.tsx
+++ src/content/Main/sections/Projects/fold_content/excellentFinanceTrackerContent.tsx
@@ -1,6 +1,27 @@
+import { GitHubIcon, LinkIcon, OfficeIcon, VBAIcon } from '../../../../../assets'
+import { TextWithIcon } from '../../../../../components'
+
 export const excellentFinanceTrackerContent = (
-	<>
-		Some text Some text Some text Some text Some text Some text Some text Some text Some text
-		Some text Some text Some text Some text Some text Some text Some text Some text Some text
-	</>
+	<div className='fold advanced-lottie-viewer'>
+		<div className='col'>
+			<div className='links'>
+				<h3>Links</h3>
+				<TextWithIcon
+					Icon={GitHubIcon}
+					href='https://github.com/JoshSilveous/excellent-finance-tracker/'
+					newWindow
+				>
+					GitHub
+				</TextWithIcon>
+			</div>
+			<div className='tech'>
+				<h3>Technologies</h3>
+				<TextWithIcon Icon={OfficeIcon}>Office</TextWithIcon>
+				<TextWithIcon Icon={VBAIcon}>VBA</TextWithIcon>
+			</div>
+		</div>
+		<div className='description'>
+			<p>text</p>
+		</div>
+	</div>
 )
