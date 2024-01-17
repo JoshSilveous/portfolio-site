8a7e31e967cb6f68a8e277beffc6c821f60ba9b2
Joshua Silveous <JoshSilveous@gmail.com>
2024-01-16 21:40:50 -0500

    hid self-photo: want to find a more professional one

--- src/content/Main/sections/AboutMe/AboutMe.tsx
+++ src/content/Main/sections/AboutMe/AboutMe.tsx
@@ -15,31 +15,32 @@ export function AboutMe() {
 		<div className='section about-me'>
 			<a className='anchor' id='about-me' />
 			<h1>About Me</h1>
-			<div className='col-container one'>
-				<div className='col-left'>
-					<h2>Background</h2>
-					<p>Hi! 👋</p>
-					<p>
-						My interest in technology has always been a theme in my life. Starting with
-						programming books in eighth grade and evolving into building computers and
-						interning at my school's IT department, my knack for technology led me to
-						pursue an Information Technology degree at the University of Toledo. There,
-						I gained exposure to various tech fields, but it was web development that
-						resonated with me the most.
-					</p>
-					<p>
-						This interest grew into a passion as I taught myself advanced JavaScript and
-						engaged in personal projects. Today, web development is more than a hobby;
-						it's the path I want to take in my professional life. With a blend of formal
-						education and self-directed learning, I'm ready to start my career in the
-						tech industry as a full-stack developer.
-					</p>
-				</div>
-				<div className='col-right'>
+			{/* <div className='col-container one'>
+				<div className='col-left'> */}
+			<div>
+				<h2>Background</h2>
+				<p>Hi! 👋</p>
+				<p>
+					My interest in technology has always been a theme in my life. Starting with
+					programming books in eighth grade and evolving into building computers and
+					interning at my school's IT department, my knack for technology led me to pursue
+					an Information Technology degree at the University of Toledo. There, I gained
+					exposure to various tech fields, but it was web development that resonated with
+					me the most.
+				</p>
+				<p>
+					This interest grew into a passion as I taught myself advanced JavaScript and
+					engaged in personal projects. Today, web development is more than a hobby; it's
+					the path I want to take in my professional life. With a blend of formal
+					education and self-directed learning, I'm ready to start my career in the tech
+					industry as a full-stack developer.
+				</p>
+			</div>
+			{/* <div className='col-right'>
 					<AboutMe1Image />
 					<p>Note to self: take a more professional photo at some point</p>
 				</div>
-			</div>
+			</div> */}
 			<div className='col-container two'>
 				<div className='col-left'>
 					<h2>Hobbies</h2>
